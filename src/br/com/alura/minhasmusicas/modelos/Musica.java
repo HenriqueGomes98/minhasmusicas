1c2457b0f8d83ab3253e8c78f5107ba02502c59a
Henrique Gomes <henrique.gomestr@gmail.com>
2024-07-18 21:19:45 -0300

    Projeto Spotfy
    Destinado a aplicar e praticar os 4 pilares da programação orienta a objeto.

---
+++ src/br/com/alura/minhasmusicas/modelos/Musica.java
@@ -0,0 +1,31 @@
+package br.com.alura.minhasmusicas;
+
+public class Musica extends Audio {
+    private String album;
+    private String genero;
+    private String cantor;
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCantor() {
+        return cantor;
+    }
+
+    public void setCantor(String cantor) {
+        this.cantor = cantor;
+    }
+}
